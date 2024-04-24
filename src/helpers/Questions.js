76c1204bbf04ca195a1785a24ea677c7265e74f5
Roshniar <roshniarunachalam183@gmail.com>
2024-04-24 20:39:02 +0530

    First Commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "What does CSS stand for?",
+    optionA: "Colorful Style Sheets",
+    optionB: "Creating style sheets",
+    optionC: "Computer Style Sheets",
+    optionD: "Cascading Style Sheets",
+    asnwer: "optionD",
+  },
+  {
+    prompt: "What is the purpose of JavaScript in web development?",
+    optionA: "To style web pages",
+    optionB: "To define the structure of web pages",
+    optionC: "To create and manipulate web page content dynamically",
+    optionD: "To define the layout of web pages",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "What does SEO stand for in web development?",
+    optionA: "Secure Encoding Options",
+    optionB: "Structured Element Organization",
+    optionC: "Source Code Examination",
+    optionD: "Search Engine Optimization",
+    asnwer: "optionD",
+  },
+];
